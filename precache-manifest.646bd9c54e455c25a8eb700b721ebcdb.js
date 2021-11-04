96870686bc4d96c829859052b4b9709fdb16d0d0
marcelineT <marcytrifunovic@gmail.com>
2021-11-04 00:49:05 -0700

    init

---
+++ precache-manifest.646bd9c54e455c25a8eb700b721ebcdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03d11466d6da4df22478",
+    "url": "/static/css/main.a0078243.chunk.css"
+  },
+  {
+    "revision": "03d11466d6da4df22478",
+    "url": "/static/js/main.03d11466.chunk.js"
+  },
+  {
+    "revision": "c8743fefa14e7527e3c4",
+    "url": "/static/js/1.c8743fef.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec02aca9e26fa14c0baa1d68f151aeef",
+    "url": "/index.html"
+  }
+];
